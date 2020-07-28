dd88d94a6b5940b0a96d70ee4c6eb04bafaadac3
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-07-28 22:47:24 +0100

    Add: setup routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Services from './components/Services/Services';
+import Navbar from './components/Layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} ></Route>
+        <Route path="/about" exact component={About} ></Route>
+        <Route path="/contact" exact component={Contact} ></Route>
+        <Route path="/services" exact component={Services} ></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
